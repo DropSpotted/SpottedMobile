a0909d30d3a5decc122b313f7192b3ecb67d3cf0
Michał Sokół <michal.sokol@deviniti.com>
2021-07-22 08:30:29 +0200

    Add initial maps images and bumped up versions

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,102 @@
 import UIKit
 import Flutter
+import MapKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    var span: CLLocationDegrees = 0.01
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let batteryChannel = FlutterMethodChannel(name: "samples.flutter.dev/battery",
+                                                  binaryMessenger: controller.binaryMessenger)
+        batteryChannel.setMethodCallHandler({
+            [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            // Note: this method is invoked on the UI thread.
+            guard call.method == "getBatteryLevel" else {
+                result(FlutterMethodNotImplemented)
+                return
+            }
+            
+            if let args = call.arguments as? Dictionary<String, Any>,
+                let lat = args["lat"] as? Double,
+                let lon = args["lon"] as? Double {
+                DispatchQueue.main.async {
+                    self?.receiveBatteryLevel(result: result, lat: lat, lon: lon)
+    //                    self.imageView.image = image
+                    }
+              } else {
+                result(FlutterError.init(code: "bad args", message: nil, details: nil))
+              }
+            
+            var args = call.arguments as? Dictionary<String, Any>
+            
+//            DispatchQueue.main.async {
+//                self?.receiveBatteryLevel(result: result)
+////                    self.imageView.image = image
+//                }
+//            self?.receiveBatteryLevel(result: result)
+        })
+        
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    private func receiveBatteryLevel(result: @escaping FlutterResult, lat: Double, lon: Double) {
+        let coordinates = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        let region = MKCoordinateRegion(
+            center:coordinates,
+            span: MKCoordinateSpan(
+                latitudeDelta: self.span,
+                longitudeDelta: self.span
+            )
+        )
+        
+        // Map options.
+        let mapOptions = MKMapSnapshotter.Options()
+        mapOptions.region = region
+//        mapOptions.size = CGSize(width: 300, height: 300)
+//        mapSnapshotOptions.showsBuildings = true
+        
+        // Create the snapshotter and run it.
+        let snapshotter = MKMapSnapshotter(options: mapOptions)
+        
+        snapshotter.start {  (snapshotOrNil, errorOrNil)  in
+            if let error = errorOrNil {
+                print(error)
+//                result(nil)
+                return
+            }
+            if let snapshot = snapshotOrNil {
+                var xd = snapshot.image;
+                var imageData = xd.jpegData(compressionQuality: 1.0)
+                print("ass")
+                if let imageFromData =  imageData {
+                    var flutterType = FlutterStandardTypedData(bytes: imageFromData)
+                    result(flutterType) // (719.0, 808.0)
+                   }
+                
+                
+
+//                self.snapshotImage = snapshot.image
+            }
+        }
+        
+        
+//        result(Int(666))
+        //        let device = UIDevice.current
+        //        device.isBatteryMonitoringEnabled = true
+        //        if device.batteryState == UIDevice.BatteryState.unknown {
+        //            result(Int(666))
+        //        } else {
+        //            result(Int(device.batteryLevel * 100))
+        //        }
+    }
+    
+    
 }
--- lib/pages/favourites/favourites_page.dart
+++ lib/pages/favourites/favourites_page.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:apple_maps_flutter/apple_maps_flutter.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotted/injector_container.dart';
@@ -52,7 +54,11 @@ class FavouriteBody extends StatelessWidget {
           (result) => ListView.builder(
             itemBuilder: (context, index) {
               if (Platform.isIOS) {
-                return AppleTiles();
+                return MyCont(
+                  lat: result[index].geoLocationCoords.coordinates.first,
+                  lon: result[index].geoLocationCoords.coordinates.last,
+                );
+                // return AppleTiles();
               } else if (Platform.isAndroid) {
                 return GoogleMaps(
                   lat: result[index].geoLocationCoords.coordinates.first,
@@ -70,6 +76,70 @@ class FavouriteBody extends StatelessWidget {
         ),
       );
     }
+  }
+}
+
+class MyCont extends StatefulWidget {
+  MyCont({
+    Key? key,
+    required this.lat,
+    required this.lon,
+  }) : super(key: key);
+
+  final double lat;
+  final double lon;
+
+  @override
+  _MyContState createState() => _MyContState();
+}
+
+class _MyContState extends State<MyCont> {
+  static const platform = const MethodChannel('samples.flutter.dev/battery');
+
+  String _batteryLevel = 'Unknown battery level.';
+  Uint8List? imageList;
+
+  Future<Uint8List?> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final result = await platform.invokeMethod('getBatteryLevel', {'lat': widget.lat, 'lon': widget.lon});
+      batteryLevel = 'Battery level at $result % .';
+      setState(() {
+        imageList = result;
+      });
+      return result;
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FutureBuilder<Uint8List?>(
+            initialData: null,
+            future: _getBatteryLevel(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data != null) {
+                return Image.memory(snapshot.data!);
+              } else {
+                return SizedBox();
+              }
+            }),
+        // if (imageList != null) Image.memory(imageList!),
+        // TextButton(
+        //   onPressed: () {
+        //     _getBatteryLevel();
+        //   },
+        //   child: Text('dasda'),
+        // ),
+      ],
+    );
   }
 }
 
